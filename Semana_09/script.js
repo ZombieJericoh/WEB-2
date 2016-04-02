a07e4080a3e75dc1d09a91a9432be92e761b2907
Eric Zapata <admin@ZombieJericohs-MacBook-Pro.local>
2016-04-01 23:26:09 -0400

    Semana_09

---
+++ Semana_09/script.js
@@ -0,0 +1,29 @@
+var main = function() {
+  $('.btn').click(function() {
+    var post = $('.status-box').val();
+    $('<li>').text(post).prependTo('.posts');
+    $('.status-box').val('');
+    $('.counter').text('180');
+    $('.btn').addClass('disabled'); 
+  });
+  
+  $('.status-box').keyup(function() {
+    var postLength = $(this).val().length;
+    var charactersLeft = 180 - postLength;
+    $('.counter').text(charactersLeft);
+  
+    if(charactersLeft < 0) {
+      $('.btn').addClass('disabled'); 
+    }
+    else if(charactersLeft == 180) {
+      $('.btn').addClass('disabled');
+    }
+    else {
+      $('.btn').removeClass('disabled');
+    }
+  });
+  
+  $('.btn').addClass('disabled');
+}
+
+$(document).ready(main);
